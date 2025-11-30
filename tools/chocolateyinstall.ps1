e3b78f93a6f62654f54488b993093a76437f1805
Valtoni Boaventura <valtoni@gmail.com>
2025-11-30 11:15:03 -0500

    Gaphor Build 3.2.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 $ErrorActionPreference = 'Stop'
 
 $packageName = 'gaphor'
-$version     = '3.1.0'
+$version     = '3.2.0'
 
 # Official Windows installer (single 64-bit EXE)
 $url64       = "https://github.com/gaphor/gaphor/releases/download/$version/gaphor-$version-installer.exe"
 
 # Official SHA256 of the EXE
-$checksum64  = '88F3A13A8038811D578F9E8E3F5041B5DD2B8F6208D7DD3E7F9286FFE61D3EDC'
+$checksum64  = 'C6076A89B3FF16318A32F4BA0F1872B258E82E50F93C9447C17FB3D623B4DF91'
 
 $packageArgs = @{
   packageName    = $packageName
